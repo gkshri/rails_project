42cb199a78624d07ee6bd0b8dc50a705634e13b6
Gkshri <gokulshrikanth@outlook.com>
2021-02-11 12:43:15 +0530

    formatted string

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+    def toreadable
+        is_completed= completed ? "[X]":"[0]"
+        "#{id}.#{todo_text}.#{is_completed}.#{due_date.to_s(:short)}"
+    end
 end
