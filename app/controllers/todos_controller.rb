7a88db7cd679d0df5a9b2f9fe7bc1c0e8514bdf2
Gkshri <gokulshrikanth@outlook.com>
2021-02-11 12:50:39 +0530

    find specific todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,10 @@ class TodosController < ApplicationController
     def index    
         render plain: Todo.all.map{|todo| todo.toreadable}.join("\n")
     end
+    def show
+        id= params[:id]
+        #render plain:"the id was.#{id}"
+        todo=Todo.find(id)
+        render plain: todo.toreadable
+    end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -3,4 +3,5 @@ class Todo < ActiveRecord::Base
         is_completed= completed ? "[X]":"[0]"
         "#{id}.#{todo_text}.#{is_completed}.#{due_date.to_s(:short)}"
     end
+
 end
